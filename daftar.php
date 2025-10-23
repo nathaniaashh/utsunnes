e82a2d04121aa27fa0794a640535673fef946bc3
nathaniaashh <thaniaisyah@students.unnes.ac.id>
2025-10-24 04:45:22 +0700

    commit pertama thania

--- daftar.php
+++ daftar.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>::Data Registrasi::</title>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
         <style type="text/css">
             body{
                 display: flex;
